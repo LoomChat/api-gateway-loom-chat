c529de607423da2275440b98e517970cdade943a
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2025-12-14 22:43:14 +0500

    Set up a simple custom logger and log methods.

---
+++ pkg/log/logs.go
@@ -0,0 +1,28 @@
+package log
+
+import (
+	"fmt"
+	"context"
+)
+
+var ctx = context.Background()
+
+func Debug(format string, args ...any) {
+	logger.Debug(fmt.Sprintf(format, args...))
+}
+
+func Info(format string, args ...any) {
+	logger.Info(fmt.Sprintf(format, args...))
+}
+
+func Warn(format string, args ...any) {
+	logger.Warn(fmt.Sprintf(format, args...))
+}
+
+func Error(format string, args ...any) {
+	logger.Error(fmt.Sprintf(format, args...))
+}
+
+func Fatal(format string, args ...any) {
+	logger.Log(ctx, LevelFatal, fmt.Sprintf(format, args...))
+}
